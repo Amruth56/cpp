6bd61e9195000103be5eb03986b787539d8009fb
Amruth <ammumandappa@gmail.com>
2022-06-13 00:11:54 +0530

    .

---
+++ practice/var.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name="Raju";
+    int age = 45;
+    int phone_number;
+
+    cout << "The name of the client is " << name << endl;
+    cout<< "And his age is " << age <<endl;
+
+
+    cout << "Enter the phone number :"<< endl;
+    cin >> phone_number ;
+    cout<<"The phone number of the client is " <<phone_number << endl;
+
+
+}
---
+++ practice/var2.cpp
@@ -0,0 +1,30 @@
+//  ID card details
+
+#include<iostream>
+using namespace std;
+
+int main(){
+string name ;
+string place;
+int phone_number;
+string branch;
+char section;
+
+cout<<"Enter your name :" ;
+cin>> name;
+
+cout<<"\rEnter your place :" ;
+cin>> place;
+
+cout<<"\rEnter your phone_number :";
+cin>> phone_number ;
+
+cout<<"\rEnter your branch :";
+cin>>branch;
+
+cout<<"\rEnter your section :";
+cin>>section;
+
+cout<< "\n\nName :"<<name <<"\nplace :" <<place <<"\nphone_number :" <<phone_number <<"\nbranch :" <<branch <<"\nsection :"<<section ;
+
+}
