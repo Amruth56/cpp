df65e9245470e074ea4637d073c87c7d7453f570
Amruth <ammumandappa@gmail.com>
2022-06-13 11:50:34 +0530

    .

--- 11_if_statements.cpp
+++ 11_if_statements.cpp
@@ -18,3 +18,27 @@ int main()
         cout<<"You are either short or you are a female";
     }
 }
+
+
+
+#include <iostream>
+using namespace std;
+
+void cond(bool ismale, bool istall){
+    if (ismale && istall){
+        cout<<"You area tall male ";
+    }
+    else if (ismale &&! istall){
+        cout<<"You are a short male ";
+    }
+    else if (istall &&! ismale){
+        cout<<"You are a tall women ";
+    }
+    else{
+        cout<<"you are either short or you are a women ";
+    }
+}
+
+int main(){
+    cond(true,true);
+}
--- practice/numbers1.cpp
+++ practice/numbers1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    
+void cond(bool ismale, bool istall){
+    if (ismale && istall){
+        cout<<"You area tall male ";
+    }
+    else if (ismale &&! istall){
+        cout<<"You are a short male ";
+    }
+    else if (istall &&! ismale){
+        cout<<"You are a tall women ";
+    }
+    else{
+        cout<<"you are either short or you are a women ";
+    }
+}
+
+int main(){
+    cond(true,true);
 }
